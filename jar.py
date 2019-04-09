f2025f4d9e44840f7be827450f201ff06144faa5
Marissa Baden <badenm42@gmail.com>
2019-04-09 06:35:11 -0600

    Allow selection by location.

--- jar.py
+++ jar.py
@@ -8,26 +8,58 @@ class Book:
         self.media = info[14]
         self.tags = info[28].split(",")
         self.collections = info[29].split(",")
+
+        # convoluted location information 
+
+        # to be safe if there is missing information
+        self.apartment = True
+        self.home = True
+
         if(self.media != "Ebook"):
             shelves = [s for s in self.tags if "Shelf" in s]
+            # if there's a shelf catalogued
             if len(shelves) > 0:
                 self.shelf = shelves[0][7:]
+                # if it's in my apartment
+                if self.shelf == "Home":
+                    self.apartment = True
+                    self.home = False
+                # if it's not in my apartment
+                else:
+                    self.apartment = False 
+                    self.home = True 
+            # if there's no shelf catalogued
             else:
+                # if it's hypothetically shelved somewhere
                 if "Shelved" in self.tags:
                     self.shelf = "not catalogued"
+                    # show up for both locations just to be safe
+                    self.home = True
+                    self.apartment = True
                 else: 
                     self.shelf = "None"
             boxes = [b for b in self.tags if "Box " in b]
+            # if there's a box catalogued
             if len(boxes) > 0:
                 self.box = boxes[0][5:]
+                # no boxes in my apartment
+                self.apartment = False 
+                self.home = True 
             else:
+                # if it's hypothetically boxed somewhere
                 if "Boxed" in self.tags:
                     self.box = "not catalogued"
+                    # show up for both locations just to be safe
+                    self.home = True
+                    self.apartment = True
                 else: 
                     self.box = "None"
         else: 
             self.shelf = "None"
             self.box = "None"
+            # assume I have my ereader
+            self.home = True
+            self.apartment = True
         
     def __str__(self):
         locationString = "Unknown"
@@ -40,7 +72,7 @@ class Book:
                 else:
                     locationString = "At home on Shelf " + self.shelf
             if self.box != "None":
-                if self.shelf == "not catalogued":
+                if self.box == "not catalogued":
                     locationString = "Boxed somewhere"
                 else:
                     locationString = "At home in Box " + self.box
@@ -67,9 +99,25 @@ with open('tbr.csv', 'r') as csvfile:
         except:
             print("\nInvalid number of books, please enter a number\n")
 
-    books = random.sample(range(0, total-1), numBooks)
+    while True:
+        try:
+            locationChar = str(input("From where (please enter a letter)?\nA) Apartment\nB) Home\nC) Anywhere\n")).upper()
+            if locationChar == "A" or locationChar == "B" or locationChar == "C":
+                print()
+                break
+            else: 
+                print("\nInvalid character, please enter A, B, or C\n")
+        except:
+                print("\nInvalid input, please enter A, B, or C\n")
+
+    if locationChar == "A":
+        subtbr = [b for b in totaltbr if b.apartment == True]
+    elif locationChar == "B":
+        subtbr = [b for b in totaltbr if b.home == True]
+    else:
+        subtbr = totaltbr
     
-    subtbr = totaltbr
+    books = random.sample(range(0, len(subtbr) - 1), numBooks)
 
     print("--------------------------------")
     for book in books:
